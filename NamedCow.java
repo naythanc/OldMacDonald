95dac6dc4552249c1c101faab2bf51f83e67d3a6
Nathan Chow <Nathan@Nathans-MacBook-Air-2.local>
2017-11-09 12:34:00 -0800

    afsbj

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,19 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+ private String myName;
+   private String myType;     
+     private String mySound; 
+  public NamedCow(String type, String name, String sound){
+  myName = name;
+           myType = type;         
+         mySound = sound;  
+  }
+       public NamedCow()     {         
+         myType = "unknown";         
+         mySound = "unknown";
+         myName = "unknown";
+     }     
+  public String getName(){
+  return myName;
+  }
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,9 @@
 
 void setup()
 {
-	//your variable declarations here
+	
+	Farm Thing = new Farm();
+	Thing.animalSounds();
+
 }
 
